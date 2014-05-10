5fafc1448dde6fa0e5dc6db27608ad8225a19daf
Noam Postavsky <npostavs@users.sourceforge.net>
2014-05-09 22:02:02 -0400

    don't allow local vars to change whitespace-style
    el-get .dir-locals.el remove tab from the styles, and it's always really
    annoying.

--- init.el
+++ init.el
@@ -511,12 +511,13 @@
 ;;; safe locals
 (put 'Package 'safe-local-variable #'symbolp)
 (put 'require-final-newline 'safe-local-variable #'symbolp)
-(put 'whitespace-style 'safe-local-variable #'listp)
 (put 'whitespace-line-column 'safe-local-variable #'integerp)
 (put 'emacs-lisp-docstring-fill-column 'safe-local-variable #'integerp)
 (put 'lexical-binding 'safe-local-variable #'booleanp)
 (setq enable-local-eval nil)
 
+;;; unsafe/annoying locals
+(setq ignored-local-variables '(whitespace-style))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
