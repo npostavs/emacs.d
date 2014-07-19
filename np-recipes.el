0692b1bea37b7ace7aa3f429017c0f56aa99e6c0
Noam Postavsky <npostavs@users.sourceforge.net>
2014-07-19 17:28:49 -0400

    update some packages
    el-get, git-modes, magit, lua-mode (adds ample-regexps), yasnippet

--- np-recipes.el
+++ np-recipes.el
@@ -4,17 +4,18 @@
            :description "Anaphoric expressions for Emacs Lisp, providing implicit temporary variables."
            :type github
            :pkgname "rolandwalker/anaphora")
+    (:name ample-regexps                   :checkout "f24c5991927c8eb180d9f1d1614da238daba6423")
     (:name dash                            :checkout "8dfa9b6e6df6162ee6f0271a23276cf1995667ab"
            :branch "2.6.0")
     (:name diminish                        :checkout "73669b69e5f9a0c9261c5b53624329d0e24d1ed8")
     (:name dired-details                   :checkout "3de7e19ae874dac03edf9951648a1f11f52dead6")
-    (:name el-get                          :checkout "50ec866d28ef4656706408bdecf20fca13299c7b"
+    (:name el-get                          :checkout "4f0e1e266704ef039ad5f147f4c6ffc00c1fc628"
            :pkgname "npostavs/el-get" :branch "origin/current")
     (:name elisp-slime-nav                 :checkout "1a2cb6a832635bde694fd25cc6dce2962aad3807"
            :description "Slime-style navigation of Emacs Lisp source with M-. & M-,"
            :type github
            :pkgname "purcell/elisp-slime-nav")
-    (:name git-modes                       :checkout "d19ee56e17d2f1bac87ff81e864b880e8f9e5fca"
+    (:name git-modes                       :checkout "07bfeb2cc5f7c0a656655c764816b2378767f88b"
            :type github :pkgname "magit/git-modes" :branch "origin/next")
     (:name htmlize                         :checkout "1e48ccdfc3aec0f80323036578c232300673a64a")
     (:name i3-emacs                        :checkout "055510298fe2d5b52a346aebcec859af9b55ac3c"
@@ -24,8 +25,8 @@
     (:name ido-complete-space-or-hyphen    :checkout "3fe1fe1e1a743f8deb8f4025977647afecd58f14"
            :description "Make ido completes like built-in M-x does"
            :type github :pkgname "doitian/ido-complete-space-or-hyphen")
-    (:name lua-mode                        :checkout "67a90221b4b82c559d62275e73dd966ff5503ed3")
-    (:name magit                           :checkout "548262dc3659d197efc6fd064e91b7a8d890546b"
+    (:name lua-mode                        :checkout "2453e370ca39f38fced67a6d2db462aaea110f22")
+    (:name magit                           :checkout "901a7b94abe270a8a4aa605de6a62fa3a5049bb2"
            :branch "origin/current" :depends (dash)
            :pkgname "npostavs/magit" :autoloads t :build nil)
     (:name multiple-cursors                :checkout "54e408fc682d968ad46846d8bff079cd704ff6fe")
@@ -47,7 +48,7 @@
            :type elpa)
     (:name undo-tree                       :checkout "bc9d09555f5aeac6ac4684d748be763f64a7d80a")
     (:name use-package                     :checkout "02833c292899d406c76233da255e3304c4ab24f3")
-    (:name yasnippet                       :checkout "763f5faa14614b207d65e03594b80ba662f47efc"))
+    (:name yasnippet                       :checkout "e53ec1e0c3611c3a0f4ac6228aab041a82b9d3bb"))
  )
 
 
